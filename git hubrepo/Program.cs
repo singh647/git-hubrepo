b09855ffdce944ff8fad1d261df08f8bf640e956
singh647 <39274143+singh647@users.noreply.github.com>
2019-03-28 16:59:50 -0400

    459

--- git hubrepo/Program.cs
+++ git hubrepo/Program.cs
@@ -1,28 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace PeterIsTheRulerOfIT
+namespace c0723889
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Peter Rules the IT Universe!");
-            Download();
-            Console.ReadLine();
-        }
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Peter Rules the IT Universe!");
+Download();
+Console.ReadLine();
+}
 
-        static void Download()
-        {
-            Task.Run(() => {
-                Thread.Sleep(3000);
-                Console.WriteLine("Download Complete ...");
-            }
-            );
-        }
-    }
+static async void Download()
+{
+await Network.Download();
+Console.WriteLine("Download Complete"); 
+}
+class Network
+{
+static public Task Download()
+{
+return Task.Run( ()=> Thread.Sleep(3000));
+}
+}
+}
 }
